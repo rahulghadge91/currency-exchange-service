cea1dcca6977816879041d5125b2f1c36332e1e3
Rahul Ghadge <Rahul_Ghadge@horizonblue.com>
2019-12-31 01:04:19 +0530

    currency exchange commit

--- src/main/java/com/rahul/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/rahul/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,19 +1,12 @@
 package com.rahul.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.cloud.config.environment.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,31 +22,27 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 @RestController
 public class CurrencyExchangeController {
 
-	@Autowired
-	private Environment environment;
+	private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	
 	@Autowired
 	private ExchangeValueResposity repository;
 	
-	double defaultConversion =10;
-	
-	//@HystrixCommand(fallbackMethod="fallbackRetriveExchangeValue")
-	@HystrixCommand(commandKey = "APPLY-DISCOUNT-COMMAND", fallbackMethod = "fallbackRetriveExchangeValue", threadPoolKey = "fallbackRetriveExchangeValue",
-			commandProperties = {
-			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
-			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60") })
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public double retriveExchangeValue(@PathVariable String from,@PathVariable String to) {
-		ExchangeValue exchangeValue= repository.findByFromAndTo(from, to);
-	
+		ExchangeValue exchangeValue=null;
+		try {
+		 exchangeValue= repository.findByFromAndTo(from, to);
+		}
+		catch(Exception e){
+			logger.error(e.getMessage());
+			throw e;
+		}
 		//exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
         //System.out.println(exchangeValue.getCoversionMultiple());
 		return exchangeValue.getCoversionMultiple();
 	}
-		public double fallbackRetriveExchangeValue(String from,String to)
-		{
-			return defaultConversion;
-		}
+
 		
 	@PostMapping("/currency-exchange/conversion")
 	public ResponseEntity<Object> createCurrencyExchange(@RequestBody ExchangeValue exchangeValue)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
-values(1001, 'USD', 'INR', 65, 0),
-(1002, 'EUR', 'INR', 75, 0),
-(1003, 'AUD', 'INR', 25, 0);
+values(1001, 'USD', 'INR', 71, 0),
+(1002, 'EUR', 'INR', 79, 0),
+(1003, 'AUD', 'INR', 50, 0);
